90522c292e9701f7638d279142e5cc601c698b08
Unknown <gael.pouger@gmail.com>
2018-09-03 21:38:28 +0200

    Add some utility functions

--- IceSL/utils.lua
+++ IceSL/utils.lua
@@ -31,6 +31,21 @@ function center_and_scale_shape(shape, diam, height)
     return shape
 end
 
+function center_text(text, diam, height, inverted, tracking, font_file)
+	inverted = inverted or false
+	tracking = tracking or 10
+	font_file = font_file or "ARIALBD.TTF"
+    font_path = Path .. font_file
+    f = font(font_path)
+    txt = f:str(text, tracking)
+	
+	if inverted then
+		txt = rotate(0, 180, 0)*txt
+	end
+
+    return center_and_scale_shape(txt, diam, height)
+end
+
 function rounded_box(w, l, h, r)
     -- produces a box with flat top and bottom, and rounded corners with radius r
     corner_neg = difference(cube(r, r, h), translate(v(r/2, r/2, 0))*cylinder(r, h))
@@ -43,3 +58,40 @@ function rounded_box(w, l, h, r)
     c = difference(c, translate(v(x_offs, y_offs, 0))*rotate(180, Z)*corner_neg)
     return c
 end
+
+function extrude_svg(svg_file, extrude_height)
+	-- get all the contours from an SVG file and transform them to a single 3D shape
+	svg_shapes = svg_ex(svg_file, 90)
+	svg_shape = linear_extrude(v(0, 0, extrude_height), svg_shapes[1]:outline())
+	for i,contour in pairs(svg_shapes) do
+		if i > 1 then
+			svg_shape = union(svg_shape, linear_extrude(v(0,0,extrude_height),contour:outline()))
+		end
+	end
+	
+	return svg_shape
+end
+
+function group_grid(shapes, nb_col, spacing)
+    max_size_x=0
+    max_size_y=0
+    for i, shape in ipairs(shapes) do
+        s_bbox = bbox(shape)
+        max_size_x = math.max(s_bbox:extent().x, max_size_x)
+        max_size_y = math.max(s_bbox:extent().y, max_size_y)
+    end
+
+    x_step = max_size_x + spacing
+    y_step = max_size_y + spacing
+	
+	s = cube(0, 0, 0)
+
+    for i, shape in ipairs(shapes) do
+        x = (i-1) % nb_col
+        y = math.floor((i-1) / nb_col)
+		s = union(s, translate(x*x_step, y*y_step, 0) * center_shape(shape))
+    end
+	
+	return s
+end
+

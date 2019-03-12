ad61ec70c22a3f5d0dc0e4281998e3bbed55dae1
Gael Pouger <gpouger@github.com>
2019-03-12 17:54:05 +0100

    Reworked number tokens generation for 2-step surface dual color printing

--- IceSL/catan/numbers.lua
+++ IceSL/catan/numbers.lua
@@ -4,9 +4,12 @@ dot_rad = 0.75
 dots_spacing = 2.5
 dots_y_offs = 9.5
 letter_y_offs = 9
-indent = 0.8
+indent = 0.4
 total_height = 2.4
 total_rad = 13
+
+grid_columns = 4
+grid_spacing = 2
 
 numbers_base = {{'A', '5', 4},
                {'B', '2', 1},
@@ -59,33 +62,80 @@ numbers_ext = {{'A', '2', 1},
         }
 
 function number_token(letter, number, nb_dots)
-  shape = cylinder(total_rad, total_height)
+  base_shape = cylinder(total_rad, total_height)
   
   dots = {}
   for i=1,nb_dots do
-    table.insert(dots, translate((i-1)*dots_spacing, -dots_y_offs, total_height-indent)*cylinder(dot_rad, 1))
+    table.insert(dots, translate((i-1)*dots_spacing, -dots_y_offs, total_height-indent)*cylinder(dot_rad, indent))
   end
   dots = union(dots)
   dots = translate(-bbox(dots):extent().x/2+dot_rad, 0, 0)*dots
   
-  number = translate(0, -1, total_height-indent)*xy_scale_to_y(center_text(number, 3, 1, false, 1, "arlrdbd.ttf"), 13)
+  number = translate(0, -1, total_height-indent)*xy_scale_to_y(center_text(number, 3, indent, false, 1, "arlrdbd.ttf"), 13)
 
-  letter = translate(0, letter_y_offs, total_height-indent)*center_text(letter, 4, 1, false, 10, "arlrdbd.ttf")
+  letter = translate(0, letter_y_offs, total_height-indent)*center_text(letter, 4, indent, false, 10, "arlrdbd.ttf")
   
-  shape = difference(shape, dots)
-  shape = difference(shape, number)
-  shape = difference(shape, letter)
+  txt = union({dots, number, letter})
 
-  return shape
+  token = {}
+  token.white = difference(base_shape, txt)
+  token.black = txt
+
+  return token
 end
 
 function get_tokens(tok_list)
   tokens = {}
   for i, num in ipairs(tok_list) do
-    table.insert(tokens, number_token(num[1], num[2], num[3]))
+    tok = number_token(num[1], num[2], num[3])
+    tok.offs = group_grid_offset(i, grid_columns, grid_spacing, total_rad*2, total_rad*2)
+    table.insert(tokens, tok)
   end
   return tokens
 end
 
---emit(group_grid(get_tokens(numbers_base), 4, 2))
-emit(group_grid(get_tokens(numbers_ext), 4, 2))
+function emit_tokens_white(tok_list)
+  for i, tok in ipairs(tok_list) do
+    offs_to_center = xy_offs_to_center(tok.white)
+    emit(translate(tok.offs.x, tok.offs.y, 0)*translate(offs_to_center)*rotate(180, Y)*tok.white, 0)
+  end
+end
+
+function emit_tokens_black(tok_list)
+  for i, tok in ipairs(tok_list) do
+    offs_to_center = xy_offs_to_center(tok.white)
+    emit(translate(tok.offs.x, tok.offs.y, 0)*translate(offs_to_center)*rotate(180, Y)*tok.black, 1)
+  end
+end
+
+function set_params_black()
+  set_setting_value("num_shells_0", 0)
+  set_setting_value("num_shells_1", 2)
+  set_setting_value("cover_thickness_mm_0", 0)
+  set_setting_value("cover_thickness_mm_1", 0.6)
+  set_setting_value("infill_percentage_0", 0)
+  set_setting_value("infill_percentage_1", 20)
+  set_setting_value("print_perimeter_0", false)
+  set_setting_value("print_perimeter_1", true)
+  set_setting_value("z_lift", 0)
+  set_setting_value("first_layer_print_speed_mm_per_sec", 10)
+end
+
+function set_params_white()
+  set_setting_value("num_shells_0", 2)
+  set_setting_value("num_shells_1", 0)
+  set_setting_value("cover_thickness_mm_0", 0.6)
+  set_setting_value("cover_thickness_mm_1", 0)
+  set_setting_value("infill_percentage_0", 20)
+  set_setting_value("infill_percentage_1", 0)
+  set_setting_value("print_perimeter_0", true)
+  set_setting_value("print_perimeter_1", false)
+  set_setting_value("z_lift", 0.5)
+  set_setting_value("first_layer_print_speed_mm_per_sec", 10)
+end
+
+emit_tokens_black(get_tokens(numbers_ext))
+emit_tokens_white(get_tokens(numbers_ext))
+
+set_params_black()
+set_params_white()
